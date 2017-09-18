8912ceabe65f695b16ab295d25d2bc7a30f1d0e5
khashashin <khashashin@gmail.com>
2017-09-18 15:23:19 +0200

    This reverts commit ec02c97c606b1155cd606e1243c762ad21100b3c.

--- feedler/wagtail_hooks.py
+++ feedler/wagtail_hooks.py
@@ -4,7 +4,6 @@ from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register)
 
 from .models import Entry, Stream
-from .models.admin import ExportModelAdminMixin
 
 class EntryModelAdmin(ModelAdmin):
     model = Entry
@@ -27,9 +26,3 @@ class StreamModelAdmin(ModelAdmin):
     list_display = ('title', 'ident')
 
 modeladmin_register(StreamModelAdmin)
-
-class EntryModelAdmin(ExportModelAdminMixin, ModelAdmin):
-    model = Entry
-    index_template_name = 'templates/modeladmin/feedler/entry/index.html'
-
-modeladmin_register(EntryModelAdmin)
