1d7545bbe239cea2826bb6f67d0cb07f516f9967
Oleg Lavrovsky <oleg@datalets.ch>
2017-10-13 23:22:34 +0200

    Datalets settings

--- publichealth/home/models/admin.py
+++ publichealth/home/models/admin.py
@@ -11,21 +11,22 @@ from wagtail.contrib.settings.models import BaseSetting, register_setting
 
 @register_setting
 class DataletsSettings(BaseSetting):
-    feedback_question = models.TextField(
-        help_text='Send us a question', blank=True)
-    feedback_status = models.IntegerField(
+    feedback_question = models.TextField(verbose_name='Name',
+        help_text='Who should we reply to for questions?', blank=True)
+    feedback_status = models.IntegerField(verbose_name='Rating',
         choices=(
-            (1, ':-('),
-            (2, ':-|'),
-            (3, ':-)'),
-            (4, ':-D'),
+            (1, u'★'),
+            (2, u'★'*2),
+            (3, u'★'*3),
+            (4, u'★'*4),
+            (5, u'★'*5),
         ), blank=True, null=True,
         help_text='How are you enjoying Wagtail?'
     )
-    feedback_comment = models.TextField(
-        help_text='Any general feedback', blank=True)
+    feedback_comment = models.TextField(verbose_name='Comments..',
+        help_text='Any questions or general feedback', blank=True)
     class Meta:
-        verbose_name = 'Get support'
+        verbose_name = 'Datalets'
 
 @receiver(pre_save, sender=DataletsSettings)
 def handle_save_settings(sender, instance, *args, **kwargs):
