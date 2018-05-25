b108a71ba93b947eef62b1e0011d0458337f5e41
Oleg Lavrovsky <oleg@utou.ch>
2018-05-25 16:01:03 +0200

    SPHC custom site style

--- publichealth/settings/dev.py
+++ publichealth/settings/dev.py
@@ -8,6 +8,9 @@ TEMPLATES[0]['OPTIONS']['debug'] = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGEME!!!'
 
+# SECURITY WARNING: CAREFUL! your dev site is open to the world
+ALLOWED_HOSTS = ['*']
+
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
 
 BASE_URL = 'http://localhost:8000'
