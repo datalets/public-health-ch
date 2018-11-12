0234298d19d1dae5c95477bb0c2ec61c548c93b3
Oleg Lavrovsky <oleg@utou.ch>
2018-11-12 18:56:20 +0100

    Add default WSGI config

--- publichealth/wsgi.py
+++ publichealth/wsgi.py
@@ -10,3 +10,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "publichealth.settings.production")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
